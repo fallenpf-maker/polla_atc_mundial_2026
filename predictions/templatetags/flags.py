ce652c73037760bab29b22628bb5a7a84b28c0bb
Edson Amonzabel <edsonamonzabel@MacBook-de-Edson.local>
2026-05-16 11:35:19 -0400

    Fix template loops2

--- predictions/templatetags/flags.py
+++ predictions/templatetags/flags.py
@@ -4,26 +4,86 @@ register = template.Library()
 
 FLAGS = {
 
-    'Mexico': 'mx',
-    'Sudáfrica': 'za',
-    'Corea del Sur': 'kr',
-    'Rep. Checa': 'cz',
-    'Brasil': 'br',
+    # ANFITRIONES
+    'Estados Unidos': 'us',
+    'México': 'mx',
+    'Canada': 'ca',
+    'Canadá': 'ca',
+
+    # CONMEBOL
     'Argentina': 'ar',
+    'Brasil': 'br',
+    'Uruguay': 'uy',
+    'Colombia': 'co',
+    'Ecuador': 'ec',
+    'Perú': 'pe',
+    'Chile': 'cl',
+    'Paraguay': 'py',
+    'Bolivia': 'bo',
+    'Venezuela': 've',
+
+    # UEFA
     'España': 'es',
     'Francia': 'fr',
     'Alemania': 'de',
     'Inglaterra': 'gb',
-    'Estados Unidos': 'us',
-    'Japón': 'jp',
     'Italia': 'it',
     'Portugal': 'pt',
-    'Uruguay': 'uy',
-    'Croacia': 'hr',
-    'Marruecos': 'ma',
-    'Canadá': 'ca',
-    'Australia': 'au',
     'Países Bajos': 'nl',
+    'Holanda': 'nl',
+    'Croacia': 'hr',
+    'Bélgica': 'be',
+    'Suiza': 'ch',
+    'Dinamarca': 'dk',
+    'Suecia': 'se',
+    'Noruega': 'no',
+    'Polonia': 'pl',
+    'Serbia': 'rs',
+    'Austria': 'at',
+    'Ucrania': 'ua',
+    'Turquía': 'tr',
+    'República Checa': 'cz',
+    'Rep. Checa': 'cz',
+    'Escocia': 'gb',
+    'Gales': 'gb',
+    'Hungría': 'hu',
+    'Grecia': 'gr',
+    'Rumania': 'ro',
+
+    # AFC
+    'Japón': 'jp',
+    'Corea del Sur': 'kr',
+    'Corea': 'kr',
+    'Australia': 'au',
+    'Arabia Saudita': 'sa',
+    'Irán': 'ir',
+    'Qatar': 'qa',
+    'Irak': 'iq',
+    'Emiratos Árabes Unidos': 'ae',
+    'China': 'cn',
+    'Uzbekistán': 'uz',
+
+    # CAF
+    'Marruecos': 'ma',
+    'Senegal': 'sn',
+    'Egipto': 'eg',
+    'Nigeria': 'ng',
+    'Camerún': 'cm',
+    'Ghana': 'gh',
+    'Costa de Marfil': 'ci',
+    'Túnez': 'tn',
+    'Argelia': 'dz',
+    'Sudáfrica': 'za',
+
+    # CONCACAF
+    'Costa Rica': 'cr',
+    'Panamá': 'pa',
+    'Jamaica': 'jm',
+    'Honduras': 'hn',
+    'El Salvador': 'sv',
+
+    # OFC
+    'Nueva Zelanda': 'nz',
 
 }
 
