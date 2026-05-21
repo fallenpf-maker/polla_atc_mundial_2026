762f08c608ef0ef01e9db7b7aea59e1620f2d43f
Edson Amonzabel <edsonamonzabel@MacBook-de-Edson.local>
2026-05-20 21:14:00 -0400

    Registrar Perfil en admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Perfil
 
-# Register your models here.
+# =====================================
+# PERFIL
+# =====================================
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'telefono'
+    )
